042ae4854efa3ba26dcb24c2af7f90af37444ad1
Nikhil <ambigarnikhil@gmail.com>
2021-10-04 23:04:45 +0530

    Oct 4

--- main.cpp
+++ main.cpp
@@ -61,4 +61,120 @@ int main()
 }
 */
 
+/*
+//structures
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+struct car
+{
+    int a;
+    char b;
+};
+int main()
+{
+    struct car c1;
+    cout<<sizeof(c1)<<endl;
+}
+//op: 8
+*/
 
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+/*
+//important concept
+//we can't assign string directly to the structure object so we use temp char arr and use strcpy.
+struct car
+{
+    int a;
+    char b;
+    char c[5];
+};
+int main()
+{
+    struct car c1;
+    cout<<sizeof(c1)<<endl;
+    c1.a=10;
+    c1.b='n';
+    char temp[5]="nikh";
+    strcpy(c1.c,temp);
+    cout<<c1.a<<"="<<c1.b<<"->"<<c1.c<<endl;
+}
+*/
+
+/*
+we can assign values to arrays only during declaration.
+int arr[5]={1,2,3,4,5} -> correct
+int arr[5];
+arr[5]={1,2,3,4,5}; ->wrong
+*/
+
+/*
+#include<string>
+struct car
+{
+    int a;
+    char b;
+    string c;
+};
+int main()
+{
+    struct car c1;
+    cout<<sizeof(c1)<<endl;
+    c1.a=10;
+    c1.b='n';
+    c1.c="Nikhil";
+    cout<<c1.a<<"="<<c1.b<<"->"<<c1.c<<endl;
+}
+*/
+/*
+#include<string>
+struct car
+{
+    string c;
+};
+int main()
+{
+    struct car c1;
+    cout<<sizeof(c1)<<endl;
+}
+//op:32
+*/
+
+/*
+#include<string>
+struct car
+{
+    int a;
+    char b;
+    char c[8];
+};
+int main()
+{
+    struct car c1;
+    cout<<sizeof(c1)<<endl;
+}
+//it should be 13 but we can access the address in multiples of 4.
+//op:16
+*/
+
+
+/*
+union car
+{
+    int a;
+    char b;
+    float c;
+    double d;
+};
+int main()
+{
+    union car c1;
+    cout<<sizeof(c1)<<endl;
+}
+//op:8
+*/
